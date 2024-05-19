fe21ff1e0acec9601fcc562176cf16d13b0d5edd
Alexander Strasser <alexander.strasser@protonmail.com>
2024-05-20 00:08:29 +0200

    refactor: new try

---
+++ routes/images.ts
@@ -0,0 +1,31 @@
+import { getTestImages } from "../utils";
+
+export function handleImageRoutes(
+  corsHeaders: Record<string, string>
+): Response {
+  const images = getTestImages();
+  return new Response(JSON.stringify(images), {
+    headers: { ...corsHeaders, "Content-Type": "application/json" },
+  });
+}
+
+export async function handleImageFile(
+  requestUrl: URL,
+  corsHeaders: Record<string, string>
+): Promise<Response> {
+  const filePath = `.${requestUrl.pathname}`;
+  try {
+    const file = await Bun.file(filePath).arrayBuffer();
+    return new Response(file, {
+      headers: {
+        ...corsHeaders,
+        "Content-Type": "image/png",
+      },
+    });
+  } catch (error) {
+    return new Response(`Error fetching file: ${filePath} not found`, {
+      status: 404,
+      headers: corsHeaders,
+    });
+  }
+}
